1729423303a7a2a1618ffb49129cee39afea0b15
HDTran91 <khoaiga0103@gmail.com>
2025-06-19 02:15:25 -0500

    using tanstack query for login

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,8 +1,33 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { EntityError } from "@/lib/http"
 import { clsx, type ClassValue } from "clsx"
+import { UseFormSetError } from "react-hook-form"
+import { toast } from "sonner"
 import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export const handleErrorApi = ({
+  error,
+  setError,
+}: {
+  error: any
+  setError?: UseFormSetError<any>
+  duration?: number
+}) => {
+  if (error instanceof EntityError && setError) {
+    error.payload.errors.forEach((item) => {
+      setError(item.field, {
+        type: 'server',
+        message: item.message
+      })
+    })
+  } else {
+    toast.error(
+      error?.payload?.message || "An unexpected error occurred.")
+  }
 }
 
 export const normalizePath = (path: string) => {

8573c543596a091cf7155f325c76bed75dd38358
HDTran91 <khoaiga0103@gmail.com>
2025-07-02 03:44:29 -0500

    route handler for refresh token

--- src/apiRequests/auth.ts
+++ src/apiRequests/auth.ts
@@ -1,5 +1,5 @@
 import http from "@/lib/http";
-import { LoginBodyType, LoginResType, LogoutBodyType } from "@/schemaValidations/auth.schema";
+import { LoginBodyType, LoginResType, LogoutBodyType, RefreshTokenBodyType, RefreshTokenResType } from "@/schemaValidations/auth.schema";
 
 const authApiRequest = {
     sLogin: (body: LoginBodyType)=> http.post<LoginResType>('/auth/login', body),
@@ -16,7 +16,11 @@ const authApiRequest = {
         }
     }),
     logout: () => http.post('/api/auth/logout', null, {
-        baseUrl:''})
+        baseUrl:''}),
+    sRefreshToken: (body: RefreshTokenBodyType) => http.post<RefreshTokenResType>('/auth/refresh-token', body),
+    refreshToken: () => http.post<RefreshTokenResType>('/api/auth/refresh-token', null, {
+        baseUrl:''
+    }),
 }
 
 export default authApiRequest;
---
+++ src/app/api/auth/refresh-token/route.ts
@@ -0,0 +1,53 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import authApiRequest from "@/apiRequests/auth";
+import { cookies } from "next/headers";
+import jwt from "jsonwebtoken";
+import { HttpError } from "@/lib/http";
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export async function POST(request: Request) {
+    const cookieStore = await cookies()
+    const refreshToken = cookieStore.get('refreshToken')?.value
+    if(!refreshToken) {
+        return Response.json({
+            message: "Refresh token is required for login.",
+            }, {
+                status: 400
+            }
+        )}
+    try {
+        const {payload} = await authApiRequest.sRefreshToken({
+            refreshToken
+        })
+        const decodedAccessToken = jwt.decode(payload.data.accessToken) as {exp:number}
+        const decodedRefreshToken = jwt.decode(payload.data.refreshToken) as {exp:number}
+        cookieStore.set('accessToken', payload.data.accessToken, {
+            path: '/',
+            httpOnly: true,
+            sameSite: 'lax',
+            secure: true,
+            expires: new Date(decodedAccessToken.exp * 1000)
+        })
+        cookieStore.set('refreshToken', payload.data.accessToken, {
+            path: '/',
+            httpOnly: true,
+            sameSite: 'lax',
+            secure: true,
+            expires: new Date(decodedRefreshToken.exp * 1000)
+        })
+        return Response.json(payload)
+    }
+    catch (error: any) {
+        if(error instanceof HttpError) {
+          return Response.json(error.payload, {
+            status: error.status
+            })
+        } else {
+            return Response.json({
+                message: error.message || 'An error occurred while refreshing token.'
+            },{
+                status: 401
+            })
+        }
+    }
+}

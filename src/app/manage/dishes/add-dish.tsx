c67e12e5739016bdb0ecc49890b5fdf944c5d9f4
HDTran91 <khoaiga0103@gmail.com>
2025-07-11 00:03:58 -0500

    get all dishes

--- src/app/manage/dishes/add-dish.tsx
+++ src/app/manage/dishes/add-dish.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 'use client'
 import { Button } from '@/components/ui/button'
 import { Dialog, DialogContent, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from '@/components/ui/dialog'
@@ -9,15 +10,20 @@ import { useMemo, useRef, useState } from 'react'
 import { useForm } from 'react-hook-form'
 import { Form, FormControl, FormField, FormItem, FormMessage } from '@/components/ui/form'
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
-import { getVietnameseDishStatus } from '@/lib/utils'
+import { getVietnameseDishStatus, handleErrorApi } from '@/lib/utils'
 import { CreateDishBody, CreateDishBodyType } from '@/schemaValidations/dish.schema'
 import { DishStatus, DishStatusValues } from '@/constants/type'
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select'
 import { Textarea } from '@/components/ui/textarea'
+import { useAddDishMutation } from '@/queries/useDish'
+import { UploadMediaMutation } from '@/queries/useMedia'
+import { toast } from 'sonner'
 
 export default function AddDish() {
   const [file, setFile] = useState<File | null>(null)
   const [open, setOpen] = useState(false)
+  const addDishMutation = useAddDishMutation()
+  const uploadMediaMutation = UploadMediaMutation()
   const imageInputRef = useRef<HTMLInputElement | null>(null)
   const form = useForm<CreateDishBodyType>({
     resolver: zodResolver(CreateDishBody),
@@ -25,7 +31,7 @@ export default function AddDish() {
       name: '',
       description: '',
       price: 0,
-      image: '',
+      image: undefined,
       status: DishStatus.Unavailable
     }
   })
@@ -38,8 +44,44 @@ export default function AddDish() {
     return image
   }, [file, image])
 
+  const reset = () => {
+    form.reset()
+    setFile(null)
+  }
+
+  const submit = async (values: CreateDishBodyType ) => {
+      if(addDishMutation.isPending) return
+      try {
+        let body = values
+        if(file) {
+          const formData = new FormData()
+          formData.append('file', file)
+          const uploadImageRedult = await uploadMediaMutation.mutateAsync(formData)
+          const imageUrl = uploadImageRedult.payload.data
+          body= {
+            ...values,
+            image: imageUrl
+          }
+          const result = await addDishMutation.mutateAsync(body)
+          toast.success(result.payload.message)
+          reset()
+          setOpen(false)
+        }
+      }
+      catch (error: any) {
+        handleErrorApi({
+          error,
+          setError: form.setError,
+        })
+      }
+    }
   return (
-    <Dialog onOpenChange={setOpen} open={open}>
+    <Dialog onOpenChange={value => {
+      if(value === false) {
+        reset()
+      }
+      setOpen(value)
+    }} open={open}>
       <DialogTrigger asChild>
         <Button size='sm' className='h-7 gap-1'>
           <PlusCircle className='h-3.5 w-3.5' />
@@ -51,7 +93,10 @@ export default function AddDish() {
           <DialogTitle>Thêm món ăn</DialogTitle>
         </DialogHeader>
         <Form {...form}>
-          <form noValidate className='grid auto-rows-max items-start gap-4 md:gap-8' id='add-dish-form'>
+          <form noValidate className='grid auto-rows-max items-start gap-4 md:gap-8' id='add-dish-form'
+            onSubmit={form.handleSubmit(submit,console.log)}
+            onReset={reset}
+          >
             <div className='grid gap-4 py-4'>
               <FormField
                 control={form.control}
@@ -61,7 +106,7 @@ export default function AddDish() {
                     <div className='flex gap-2 items-start justify-start'>
                       <Avatar className='aspect-square w-[100px] h-[100px] rounded-md object-cover'>
                         <AvatarImage src={previewAvatarFromFile} />
-                        <AvatarFallback className='rounded-none'>{name || 'Avatar'}</AvatarFallback>
+                        <AvatarFallback className='rounded-none'>{name || 'Food Image'}</AvatarFallback>
                       </Avatar>
                       <input
                         type='file'
--- src/app/manage/dishes/dish-table.tsx
+++ src/app/manage/dishes/dish-table.tsx
@@ -1,5 +1,6 @@
+
 /* eslint-disable @typescript-eslint/no-unused-vars */
-/* eslint-disable @typescript-eslint/no-explicit-any */
+
 'use client'
 
 import { DotsHorizontalIcon } from '@radix-ui/react-icons'
@@ -45,6 +46,8 @@ import AutoPagination from '@/components/auto-pagination'
 import { DishListResType } from '@/schemaValidations/dish.schema'
 import EditDish from '@/app/manage/dishes/edit-dish'
 import AddDish from '@/app/manage/dishes/add-dish'
+import { useDishListQuery } from '@/queries/useDish'
+import DOMPurify from 'dompurify'
 
 type DishItem = DishListResType['data'][0]
 
@@ -91,7 +94,7 @@ export const columns: ColumnDef<DishItem>[] = [
     accessorKey: 'description',
     header: 'Mô tả',
     cell: ({ row }) => (
-      <div dangerouslySetInnerHTML={{ __html: row.getValue('description') }} className='whitespace-pre-line' />
+      <div dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(row.getValue('description')) }} className='whitespace-pre-line' />
     )
   },
   {
@@ -171,7 +174,8 @@ export default function DishTable() {
   const pageIndex = page - 1
   const [dishIdEdit, setDishIdEdit] = useState<number | undefined>()
   const [dishDelete, setDishDelete] = useState<DishItem | null>(null)
-  const data: any[] = []
+  const distListQuery = useDishListQuery()
+  const data = distListQuery.data?.payload.data ?? []
   const [sorting, setSorting] = useState<SortingState>([])
   const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([])
   const [columnVisibility, setColumnVisibility] = useState<VisibilityState>({})

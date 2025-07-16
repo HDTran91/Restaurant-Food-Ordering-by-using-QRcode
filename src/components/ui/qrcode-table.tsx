fdffce2b6bd848674fdc6789d0fb7eeb72f68497
HDTran91 <khoaiga0103@gmail.com>
2025-07-15 20:46:30 -0500

    write text on qr code by creating virtual canvas

--- src/components/ui/qrcode-table.tsx
+++ src/components/ui/qrcode-table.tsx
@@ -1,36 +1,54 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 'use client'
+import { getTableLink } from '@/lib/utils'
+import QRCode from 'qrcode'
+import { useEffect, useRef } from 'react'
 
-import { useEffect, useRef } from "react"
-import QRcode from "qrcode"
-import { getTableLink } from "@/lib/utils"
-
-export default function QrCodeTable({
-    token, tableNumber, width = 250
+export default function QRCodeTable({
+  token,
+  tableNumber,
+  width = 250
 }: {
-    token: string
-    tableNumber: number
-    width?: number
+  token: string
+  tableNumber: number
+  width?: number
 }) {
-    const canvasRef = useRef<HTMLCanvasElement | null>(null)
-
-    useEffect(() => {
-        const canvas = canvasRef.current;
-        QRcode.toCanvas(
-            canvas,
-            getTableLink({
-                token,
-                tableNumber
-            }),
-            function (error) {
-            if (error) console.error(error);
-            console.log('QR code generated!');
-        });
-    }, [token, tableNumber]);
-
-    return (
-        <canvas
-        ref = {canvasRef}
-        />
-      )
-    }
+  const canvasRef = useRef<HTMLCanvasElement>(null)
+  useEffect(() => {
+    const canvas = canvasRef.current!
+    canvas.height = width + 70
+    canvas.width = width
+    const canvasContext = canvas.getContext('2d')!
+    canvasContext.fillStyle = '#fff'
+    canvasContext.fillRect(0, 0, canvas.width, canvas.height)
+    canvasContext.font = '20px Arial'
+    canvasContext.textAlign = 'center'
+    canvasContext.fillStyle = '#000'
+    canvasContext.fillText(
+      `Bàn số ${tableNumber}`,
+      canvas.width / 2,
+      canvas.width + 20
+    )
+    canvasContext.fillText(
+      `Quét mã QR để gọi món`,
+      canvas.width / 2,
+      canvas.width + 50
+    )
+    const virtalCanvas = document.createElement('canvas')
+    QRCode.toCanvas(
+      virtalCanvas,
+      getTableLink({
+        token,
+        tableNumber
+      }),
+      {
+        width,
+        margin: 4
+      },
+      function (error) {
+        if (error) console.error(error)
+        canvasContext.drawImage(virtalCanvas, 0, 0, width, width)
+      }
+    )
+  }, [token, width, tableNumber])
+  return <canvas ref={canvasRef} />
+}

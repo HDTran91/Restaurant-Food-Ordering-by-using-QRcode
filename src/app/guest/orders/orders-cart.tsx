b07451dc78521f17ba4d7baf612109764cc849f5
HDTran91 <khoaiga0103@gmail.com>
2025-07-23 00:54:36 -0500

    set up socket io for order realtime

--- src/app/guest/orders/orders-cart.tsx
+++ src/app/guest/orders/orders-cart.tsx
@@ -1,22 +1,54 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 'use client'
 
 import { useGuestGetOrderListQuery } from '@/queries/useGuest'
-import React, { useMemo } from 'react'
+import React, { useEffect, useMemo } from 'react'
 import Image from 'next/image'
 import { Badge } from '@/components/ui/badge'
 import { formatCurrency, getVietnameseOrderStatus } from '@/lib/utils'
+import socket from '@/lib/socket'
+import { UpdateOrderBodyType, UpdateOrderResType } from '@/schemaValidations/order.schema'
 
 
 
 export default function OrdersCart() {
-    const {data} = useGuestGetOrderListQuery()
-    const orders = useMemo(() => data?.payload.data ?? [], [data])
+  const {data, refetch} = useGuestGetOrderListQuery()
+  const orders = useMemo(() => data?.payload.data ?? [], [data])
+  console.log('orders', orders)
 
-    const totalPrice = useMemo(() => {
-            return orders.reduce((total, order) => {
-                return total + order.dishSnapshot.price * order.quantity
-            }, 0)
-        }, [orders])
+  const totalPrice = useMemo(() => {
+          return orders.reduce((total, order) => {
+              return total + order.dishSnapshot.price * order.quantity
+          }, 0)
+      }, [orders])
+  useEffect(() => {
+    if (socket.connected) {
+      onConnect();
+    }
+
+    function onConnect() {
+      console.log('Socket connected', socket.id);
+    }
+
+    function onDisconnect() {
+      console.log('Disconnected from socket server');
+    }
+
+    function onUpdatedOrders(data: UpdateOrderResType['data']) {
+      refetch()
+    }
+
+    socket.on("update-orders", onUpdatedOrders)
+
+    socket.on("connect", onConnect);
+    socket.on("disconnect", onDisconnect);
+
+    return () => {
+      socket.off("connect", onConnect);
+      socket.off("disconnect", onDisconnect);
+      socket.off("update-orders", onUpdatedOrders);
+    };
+  }, []);
   return (
     <>
     {orders.map((order) => (

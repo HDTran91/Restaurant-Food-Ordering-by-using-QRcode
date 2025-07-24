5437ccef8b1a9e87ec2d675ea4c4c45b4afb6842
HDTran91 <khoaiga0103@gmail.com>
2025-07-24 03:13:09 -0500

    ok

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,25 +1,33 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { EntityError } from "@/lib/http"
-import { clsx, type ClassValue } from "clsx"
-import { UseFormSetError } from "react-hook-form"
-import { toast } from "sonner"
-import { twMerge } from "tailwind-merge"
-import jwt from "jsonwebtoken"
-import authApiRequest from "@/apiRequests/auth"
-import { DishStatus, OrderStatus, Role, TableStatus } from "@/constants/type"
-import envConfig from "@/config"
-import slugify from 'slugify'
-import { TokenPayload } from "@/types/jwt.types"
-import guestApiRequest from "@/apiRequests/guest"
-
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { EntityError } from '@/lib/http'
+import { type ClassValue, clsx } from 'clsx'
+import { UseFormSetError } from 'react-hook-form'
+import { twMerge } from 'tailwind-merge'
+import jwt from 'jsonwebtoken'
+import authApiRequest from '@/apiRequests/auth'
+import { DishStatus, OrderStatus, Role, TableStatus } from '@/constants/type'
+import envConfig from '@/config'
+import { TokenPayload } from '@/types/jwt.types'
+import guestApiRequest from '@/apiRequests/guest'
+import { format } from 'date-fns'
+import { BookX, CookingPot, HandCoins, Loader, Truck } from 'lucide-react'
+import { toast } from 'sonner'
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+/**
+ * Xóa đi ký tự `/` đầu tiên của path
+ */
+export const normalizePath = (path: string) => {
+  return path.startsWith('/') ? path.slice(1) : path
 }
 
 export const handleErrorApi = ({
   error,
   setError,
+  duration
 }: {
   error: any
   setError?: UseFormSetError<any>
@@ -33,75 +41,65 @@ export const handleErrorApi = ({
       })
     })
   } else {
-    toast.error(
-      error?.payload?.message || "An unexpected error occurred.")
+    toast.error('Lỗi: ' + (error?.payload?.message ?? 'Lỗi không xác định'), {
+      duration: duration ?? 5000
+    })
   }
-}
-
-export const normalizePath = (path: string) => {
-  if (path.startsWith("/")) {
-    return path.slice(1);
-  }
-  return path;
 }
 
 const isBrowser = typeof window !== 'undefined'
 
-export const getAccessTokenFromLocalStorage = () =>
-  isBrowser ? localStorage.getItem('accessToken') : null
+export const getAccessTokenFromLocalStorage = () => (isBrowser ? localStorage.getItem('accessToken') : null)
 
-export const getRefreshTokenFromLocalStorage = () =>
-  isBrowser ? localStorage.getItem('refreshToken') : null
+export const getRefreshTokenFromLocalStorage = () => (isBrowser ? localStorage.getItem('refreshToken') : null)
+export const setAccessTokenToLocalStorage = (value: string) => isBrowser && localStorage.setItem('accessToken', value)
 
-export const setAccessTokenToLocalStorage = (value: string) =>
-  isBrowser && localStorage.setItem('accessToken', value)
-
-export const setRefreshTokenToLocalStorage = (value: string) =>
-  isBrowser && localStorage.setItem('refreshToken', value)
-
+export const setRefreshTokenToLocalStorage = (value: string) => isBrowser && localStorage.setItem('refreshToken', value)
 export const removeTokensFromLocalStorage = () => {
   if (isBrowser) localStorage.removeItem('accessToken')
   if (isBrowser) localStorage.removeItem('refreshToken')
 }
+export const checkAndRefreshToken = async (param?: { onError?: () => void; onSuccess?: () => void }) => {
+  // Không nên đưa logic lấy access và refresh token ra khỏi cái function `checkAndRefreshToken`
+  // Vì để mỗi lần mà checkAndRefreshToken() được gọi thì chúng ta se có một access và refresh token mới
+  // Tránh hiện tượng bug nó lấy access và refresh token cũ ở lần đầu rồi gọi cho các lần tiếp theo
+  const accessToken = getAccessTokenFromLocalStorage()
+  const refreshToken = getRefreshTokenFromLocalStorage()
+  // Chưa đăng nhập thì cũng không cho chạy
+  if (!accessToken || !refreshToken) return
+  const decodedAccessToken = decodeToken(accessToken)
+  const decodedRefreshToken = decodeToken(refreshToken)
+  // Thời điểm hết hạn của token là tính theo epoch time (s)
+  // Còn khi các bạn dùng cú pháp new Date().getTime() thì nó sẽ trả về epoch time (ms)
+  const now = new Date().getTime() / 1000 - 1
+  // trường hợp refresh token hết hạn thì cho logout
+  if (decodedRefreshToken.exp <= now) {
+    removeTokensFromLocalStorage()
+    return param?.onError && param.onError()
+  }
+  // Ví dụ access token của chúng ta có thời gian hết hạn là 10s
+  // thì mình sẽ kiểm tra còn 1/3 thời gian (3s) thì mình sẽ cho refresh token lại
+  // Thời gian còn lại sẽ tính dựa trên công thức: decodedAccessToken.exp - now
+  // Thời gian hết hạn của access token dựa trên công thức: decodedAccessToken.exp - decodedAccessToken.iat
+  if (decodedAccessToken.exp - now < (decodedAccessToken.exp - decodedAccessToken.iat) / 3) {
+    // Gọi API refresh token
+    try {
+      const role = decodedRefreshToken.role
+      const res = role === Role.Guest ? await guestApiRequest.refreshToken() : await authApiRequest.refreshToken()
+      setAccessTokenToLocalStorage(res.payload.data.accessToken)
+      setRefreshTokenToLocalStorage(res.payload.data.refreshToken)
+      if (param?.onSuccess) param.onSuccess()
+    } catch (error) {
+      if (param?.onError) param.onError()
+    }
+  }
+}
 
-export const checkAndRefreshToken = async (param?:{
-      onError?: () => void
-      onSuccess?: () => void
-}) => {
-
-        const accessToken = getAccessTokenFromLocalStorage()
-        const refreshToken = getRefreshTokenFromLocalStorage()
-        if(!accessToken || !refreshToken) {
-          // If either token is missing, we don't need to refresh
-          return
-        }
-        const decodedAccessToken = decodeToken(accessToken)
-        const decodedRefreshToken = decodeToken(refreshToken)
-        const now = (new Date().getTime() / 1000) -1  // Current time in seconds
-        if(decodedRefreshToken.exp <= now) {
-          removeTokensFromLocalStorage()
-          if (param?.onError) {
-            param.onError();
-          }
-          return
-        }
-        if(decodedAccessToken.exp - now < (decodedAccessToken.exp - decodedAccessToken.iat)/3 ) {
-            try {
-                const role = decodedRefreshToken.role
-                const res = role === Role.Guest ? await guestApiRequest.refreshToken() : (await authApiRequest.refreshToken())
-                setAccessTokenToLocalStorage(res.payload.data.accessToken)
-                setRefreshTokenToLocalStorage(res.payload.data.refreshToken)
-                if (param?.onSuccess) {
-                    param.onSuccess();
-                }
-            }
-
-            catch (error) {
-                if (param?.onError) {
-                    param.onError();
-                }
-            }
-        }
+export const formatCurrency = (number: number) => {
+  return new Intl.NumberFormat('vi-VN', {
+    style: 'currency',
+    currency: 'VND'
+  }).format(number)
 }
 
 export const getVietnameseDishStatus = (status: (typeof DishStatus)[keyof typeof DishStatus]) => {
@@ -141,25 +139,38 @@ export const getVietnameseTableStatus = (status: (typeof TableStatus)[keyof type
   }
 }
 
-export const formatCurrency = (number: number) => {
-  return new Intl.NumberFormat('vi-VN', {
-    style: 'currency',
-    currency: 'VND'
-  }).format(number)
-}
-
 export const getTableLink = ({ token, tableNumber }: { token: string; tableNumber: number }) => {
   return envConfig.NEXT_PUBLIC_URL + '/tables/' + tableNumber + '?token=' + token
 }
 
-export const generateSlugUrl = ({ name, id }: { name: string; id: number }) => {
-  return `${slugify(name)}-i.${id}`
-}
-
-export const getIdFromSlugUrl = (slug: string) => {
-  return Number(slug.split('-i.')[1])
-}
-
 export const decodeToken = (token: string) => {
   return jwt.decode(token) as TokenPayload
+}
+
+export function removeAccents(str: string) {
+  return str
+    .normalize('NFD')
+    .replace(/[\u0300-\u036f]/g, '')
+    .replace(/đ/g, 'd')
+    .replace(/Đ/g, 'D')
+}
+
+export const simpleMatchText = (fullText: string, matchText: string) => {
+  return removeAccents(fullText.toLowerCase()).includes(removeAccents(matchText.trim().toLowerCase()))
+}
+
+export const formatDateTimeToLocaleString = (date: string | Date) => {
+  return format(date instanceof Date ? date : new Date(date), 'HH:mm:ss dd/MM/yyyy')
+}
+
+export const formatDateTimeToTimeString = (date: string | Date) => {
+  return format(date instanceof Date ? date : new Date(date), 'HH:mm:ss')
+}
+
+export const OrderStatusIcon = {
+  [OrderStatus.Pending]: Loader,
+  [OrderStatus.Processing]: CookingPot,
+  [OrderStatus.Rejected]: BookX,
+  [OrderStatus.Delivered]: Truck,
+  [OrderStatus.Paid]: HandCoins
 }
